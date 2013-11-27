4d3b3c6194d905c68db92ac7fa9872d42981ec60
nikolay.viguro <nv@ph-systems.ru>
2013-11-27 17:50:15 +0400

    Update database structure

--- iris-core/conf/iris-db.sql
+++ iris-core/conf/iris-db.sql
@@ -2,26 +2,30 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT = @@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS = @@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION = @@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
 
 CREATE TABLE IF NOT EXISTS `devices` (
-  `id` int(10) NOT NULL AUTO_INCREMENT,
-  `source` varchar(255) NOT NULL,
-  `uuid` varchar(255) NOT NULL,
-  `type` varchar(255) CHARACTER SET latin1 NOT NULL,
-  `manufname` varchar(255) NOT NULL,
-  `node` int(2) NOT NULL,
-  `status` varchar(50) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `zone` int(2) NOT NULL,
-  `internaltype` varchar(255) NOT NULL,
-  `productname` varchar(255) NOT NULL,
-  `internalname` varchar(255) NOT NULL,
+  `id`           INT(10)              NOT NULL AUTO_INCREMENT,
+  `source`       VARCHAR(255)         NOT NULL,
+  `uuid`         VARCHAR(255)         NOT NULL,
+  `type`         VARCHAR(255)
+                 CHARACTER SET latin1 NOT NULL,
+  `manufname`    VARCHAR(255)         NOT NULL,
+  `node`         INT(2)               NOT NULL,
+  `status`       VARCHAR(50)          NOT NULL,
+  `name`         VARCHAR(255)         NOT NULL,
+  `zone`         INT(2)               NOT NULL,
+  `internaltype` VARCHAR(255)         NOT NULL,
+  `productname`  VARCHAR(255)         NOT NULL,
+  `internalname` VARCHAR(255)         NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=186 ;
+)
+  ENGINE =InnoDB
+  DEFAULT CHARSET =utf8
+  AUTO_INCREMENT =1;
 
 -- --------------------------------------------------------
 
@@ -30,12 +34,15 @@ CREATE TABLE IF NOT EXISTS `devices` (
 --
 
 CREATE TABLE IF NOT EXISTS `log` (
-  `id` int(10) NOT NULL AUTO_INCREMENT,
-  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `level` enum('DEBUG','INFO','WARN','ERROR') NOT NULL,
-  `message` varchar(255) NOT NULL,
+  `id`      INT(10)                                NOT NULL AUTO_INCREMENT,
+  `date`    TIMESTAMP                              NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `level`   ENUM('DEBUG', 'INFO', 'WARN', 'ERROR') NOT NULL,
+  `message` VARCHAR(255)                           NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+)
+  ENGINE =InnoDB
+  DEFAULT CHARSET =utf8
+  AUTO_INCREMENT =1;
 
 -- --------------------------------------------------------
 
@@ -44,41 +51,63 @@ CREATE TABLE IF NOT EXISTS `log` (
 --
 
 CREATE TABLE IF NOT EXISTS `messages` (
-  `id` int(10) NOT NULL AUTO_INCREMENT,
-  `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `subject` varchar(255) NOT NULL,
-  `sender` varchar(255) NOT NULL,
-  `class` varchar(255) NOT NULL,
-  `json` text NOT NULL,
+  `id`      INT(10)      NOT NULL AUTO_INCREMENT,
+  `time`    TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `subject` VARCHAR(255) NOT NULL,
+  `sender`  VARCHAR(255) NOT NULL,
+  `class`   VARCHAR(255) NOT NULL,
+  `json`    TEXT         NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=7381 ;
+)
+  ENGINE =InnoDB
+  DEFAULT CHARSET =utf8
+  AUTO_INCREMENT =1;
 
 CREATE TABLE IF NOT EXISTS `modulestatus` (
-  `id` int(10) NOT NULL AUTO_INCREMENT,
-  `name` varchar(100) NOT NULL,
-  `lastseen` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `state` varchar(50) NOT NULL,
+  `id`       INT(10)      NOT NULL AUTO_INCREMENT,
+  `name`     VARCHAR(100) NOT NULL,
+  `lastseen` TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `state`    VARCHAR(50)  NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=12492 ;
+)
+  ENGINE =InnoDB
+  DEFAULT CHARSET =utf8
+  AUTO_INCREMENT =1;
 
 
 CREATE TABLE IF NOT EXISTS `scheduler` (
-  `id` int(10) NOT NULL AUTO_INCREMENT,
-  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `class` varchar(255) NOT NULL,
-  `command` varchar(255) NOT NULL,
-  `type` int(1) NOT NULL,
-  `validto` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `interval` varchar(100) NOT NULL,
-  `enabled` int(1) NOT NULL,
-  `language` varchar(20) NOT NULL,
+  `id`       INT(10)      NOT NULL AUTO_INCREMENT,
+  `date`     TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `class`    VARCHAR(255) NOT NULL,
+  `command`  VARCHAR(255) NOT NULL,
+  `type`     INT(1)       NOT NULL,
+  `validto`  TIMESTAMP    NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `interval` VARCHAR(100) NOT NULL,
+  `enabled`  INT(1)       NOT NULL,
+  `language` VARCHAR(20)  NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+)
+  ENGINE =InnoDB
+  DEFAULT CHARSET =utf8
+  AUTO_INCREMENT =1;
 
 
 INSERT INTO `scheduler` (`id`, `date`, `class`, `command`, `type`, `validto`, `interval`, `enabled`, `language`) VALUES
 (1, '2013-11-27 03:00:00', 'Say', 'Доброе утро!', 1, '0000-00-00 00:00:00', '0 0 7 ? * MON,TUE,WED,THU,FRI *', 1, 'ru');
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+CREATE TABLE IF NOT EXISTS `devicesvalues` (
+  `id`    INT(10)      NOT NULL AUTO_INCREMENT,
+  `uuid`  VARCHAR(150) NOT NULL,
+  `label` VARCHAR(255) NOT NULL,
+  `value` VARCHAR(255) NOT NULL,
+  `type`  VARCHAR(50)  NOT NULL,
+  `units` VARCHAR(50)  NOT NULL,
+  PRIMARY KEY (`id`)
+)
+  ENGINE =InnoDB
+  DEFAULT CHARSET =utf8
+  AUTO_INCREMENT =1;
+
+/*!40101 SET CHARACTER_SET_CLIENT = @OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS = @OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION = @OLD_COLLATION_CONNECTION */;
